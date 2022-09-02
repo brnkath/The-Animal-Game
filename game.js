cafa7de6f31ef7329ec8b3ddfd0311003c8243ea
brnkath <brnkath@gmail.com>
2022-09-01 23:14:07 -0500

    Second push with updates.

---
+++ game.js
@@ -0,0 +1,22 @@
+
+let dog = 1;
+let cat = 1;
+let bird = 1;
+let squirrel = 1;
+let rabbit = 1;
+let cow = 1;
+let horse = 1;
+let deer = 2;
+let fox = 3;
+let coyote = 3;
+let possum = 5;
+let raccoon = 5;
+let skunk = 5;
+let porcupine = 7;
+let bear = 20;
+let moose = 20;
+let wolf = 25;
+let bobcat = 35;
+let man = 100;
+let total = 0;
+let cemetary = total * 0;
