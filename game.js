0d160c2be0ff4707b52f1684f5f2be3387f8b24c
brnkath <brnkath@gmail.com>
2022-09-03 00:41:41 -0500

    Changed fonts and button numbers.

--- game.js
+++ game.js
@@ -6,15 +6,15 @@ let squirrel = 1;
 let rabbit = 1;
 let cow = 1;
 let horse = 1;
-let deer = 2;
-let fox = 3;
-let coyote = 3;
+let deer = 3;
+let fox = 5;
+let coyote = 5;
 let possum = 5;
 let raccoon = 5;
 let skunk = 5;
-let porcupine = 7;
-let bear = 20;
-let moose = 20;
+let porcupine = 10;
+let bear = 25;
+let moose = 25;
 let wolf = 25;
 let bobcat = 35;
 let man = 100;
@@ -24,4 +24,4 @@ let cemetary = total * 0;
 
 // Total button presses so far, and display in total section
 
-// Show "You Win" when total is equal to or greater than 100. Show "You Lose" when Cemetary is pressed.
+// Show "You Win" when total is equal to or greater than 100. Show "Oops, back to zero" when Cemetary is pressed and change total back to zero.
