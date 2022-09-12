1a323a25bcfb87803374448ddf62ac1647379fd1
brnkath <brnkath@gmail.com>
2022-09-11 21:50:58 -0500

    Add some functionality to js file

--- js/game.js
+++ js/game.js
@@ -5,23 +5,65 @@ let bird = 1;
 let squirrel = 1;
 let rabbit = 1;
 let cow = 1;
-let horse = 1;
-let deer = 3;
-let fox = 5;
-let coyote = 5;
-let possum = 5;
-let raccoon = 5;
-let skunk = 5;
-let porcupine = 10;
-let bear = 25;
-let moose = 25;
-let wolf = 25;
-let bobcat = 35;
+let horse = 2;
+let deer = 4;
+let fox = 10;
+let coyote = 10;
+let possum = 15;
+let raccoon = 15;
+let skunk = 20;
+let bear = 35;
+let moose = 45;
+let wolf = 50;
 let man = 100;
-let total = 0;
-let cemetary = total * 0;
+let totalCount = "Total = " + 0;
+let cemetary = totalCount * 0;
+// For testing
+let buttonPress = "Total = " + 5;
 
+// Start with empty array
+  // gamePresses [
+
+  // ]
 
 // Total button presses so far, and display in total section
+  // buttonPresses [
 
-// Show "You Win" when total is equal to or greater than 100. Show "Oops, back to zero" when Cemetary is pressed and change total back to zero.
+  // ]
+
+// Making a button change total text for testing
+  document.querySelector(".game-btn").onclick = function() {
+    let buttonClicked = this.id;
+    document.querySelector(".totalCount").innerHTML = "Total = " + buttonClicked;
+  }
+
+
+// Show "You Win" when total is equal to or greater than 100. Show "Oops, back to zero" when Cemetary is pressed and after short delay change total back to zero.
+  if (totalCount >= 100) {
+    document.querySelector(".totalCount").innerHTML = "You Win!";
+  }
+
+  document.querySelector(".cemetary").onclick = function() {
+    document.querySelector(".totalCount").innerHTML = "Oops, back to zero";
+    setTimeout(function() {
+      document.querySelector(".totalCount").innerHTML = "Total = 0";
+    }, 3000);
+  }
+
+// Undo previous addition to gamePresses array
+
+
+// Reset game by wiping out gamePresses array and setting total to zero
+
+
+// Add a sound and vibration on button press
+  $(".btn").click(function() {
+    let buttonSound = new Audio("snd/click-sound.mp3");
+    buttonSound.play();
+  })
+
+  document.querySelector(".btn").onclick = function() {
+    window.navigator.vibrate(300);
+  }
+
+// Silence sound and vibration based on settings toggle positions
