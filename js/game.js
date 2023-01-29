c4d04c9b02dd358a3a351466c3897c62f6b7327c
brnkath <brnkath@gmail.com>
2023-01-28 21:11:40 -0600

    Update some js contents

--- js/game.js
+++ js/game.js
@@ -25,9 +25,10 @@ let man = 100;
     if (thisButton.length !== 0) {
       for (var i = 0; i < thisButton.length; i++) {
         thisButton[i].addEventListener("click", function() {
-          let buttonClicked = this.id; // Number(moose); returns 45
+          let buttonClicked = this.id; // Number(moose); returns 45 - maybe this.value and add a value for each button instead of defining the variables in js
           let addValue = buttonClicked;
           buttonPresses.push(addValue);
+          // let totalValue = buttonPresses.reduce((total, num) => total + num, 0); //Look in git history to see what this was before
           document.querySelector(".totalCount").innerHTML = "Total = " + buttonPresses;
         })
       }
@@ -78,25 +79,27 @@ let man = 100;
     startOver();
   }
 
-// Add a sound on button press - WORKS
+// Toggle sound on button press -
   $(".btn, .form-check-input").click(function() {
     let buttonSound = new Audio("snd/click-sound.mp3");
     buttonSound.play();
-  })
+  }) //Add an if else to change the css of On or OFF associated with soundOff and soundOn classes
 
 // Add a vibration on button press on mobile devices - does not work
   // $(".btn").click(function() {
   //   window.navigator.vibrate(300);
   // })
-  const buttonClick = document.querySelectorAll(".game-btn");
+  // const buttonClick = document.querySelectorAll(".game-btn");
 
-    buttonClick.addEventListener("click", function() {
-      navigator.vibrate(100);
-    });
+  //   buttonClick.addEventListener("click", function() {
+  //     navigator.vibrate(100);
+  //   });
 
 // Silence sound based on settings toggle positions - does not work
   // if buttonSound.[play() == true {
   //   $(".soundSwitch").click;
   // }
+
+  //Maybe change settings button to just sound on/off and add a symbol for each state or highlight on or off based on the state with changes to innerHTML
 
 // Stop vibration based on settings toggle switch - does not work
